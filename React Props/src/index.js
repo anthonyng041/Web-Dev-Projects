d51ed23ec8f4f525de0a4d80622a26f92e432667
anthonyng041 <anthonyng041@gmail.com>
2024-01-03 14:06:09 -0500

    React props completed

---
+++ React Props/src/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Card(props) {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <img src={props.img} alt={props.alt} />
+      <p>{props.phone}</p>
+      <p>{props.email}</p>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <div>
+    <h1>My Contacts</h1>
+    <Card
+      name="Beyonce"
+      img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+      alt="photo of Beyonce"
+      phone="422321"
+      email="a@a.com"
+    />
+    <Card
+      name="Jack Bauer"
+      img="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
+      alt="photo of Jack Bauer"
+      phone="42221"
+      email="b@b.com"
+    />
+  </div>,
+  document.getElementById("root")
+);
